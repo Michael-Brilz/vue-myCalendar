4a8d5d9197f84f5229961ff142e506f56c7b8e15
michael.brilz <brilz.michael@web.de>
2024-11-06 22:23:16 +0100

    fix: fix export issue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-import { createApp } from 'vue';
-import App from './components/App.vue';
 import MyCalendar from './components/ScheduleForm.vue';
 
-// Vue App erstellen und mounten
-const app = createApp(App);
-
-// Register the component globally if you want to use it in any component
-app.component('ScheduleForm', MyCalendar);
-
-app.mount('#app');
+export default MyCalendar;
